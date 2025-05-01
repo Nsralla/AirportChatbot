89a912665a305db900c1a2fb0b1e6f326e98c80b
Nsralla <nsrhassan22@gmail.com>
2025-05-01 13:11:33 +0300

    init frontend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from typing import List
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, APIRouter
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from sqlalchemy.orm import  Session
 from datetime import datetime
@@ -8,7 +9,10 @@ from backend.models import User, Interaction
 from backend.schemas import interactionCreate, interactionResponse, userCreate, userResponse
 from backend.security import hash_password, verify_password
 from . import models
+from backend.auth import create_access_token, verify_token
 
+
+auth_router = APIRouter()
 
 # create the database tables
 models.Base.metadata.create_all(bind=engine)
@@ -49,3 +53,11 @@ def getUsers( db: Session = Depends(getDB)):
     return allUsers
 
 
+# # login endpoint
+# @auth_router.post("/login")
+# def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(getDB)):
+#     user = db.query(User).filter(User.email == form_data.username).first()
+#     if not user or not verify_password(form_data.password, user.password):
+#         raise HTTPException(status_code=400, detail={"message": "Invalid credentials"})
+#     access_token = create_access_token(data={"sub": user.id})
+#     return {"access_token": access_token, "token_type": "bearer"}
